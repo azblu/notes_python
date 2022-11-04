abe8406a1691b6eba4f0f85d90e61574cb5526a2
azblu <98330304+azblu@users.noreply.github.com>
2022-11-04 16:41:27 -0500

    Create operations.py

---
+++ array/operations.py
@@ -0,0 +1,20 @@
+fruits = list()
+fruits = ['Banana', 'Cherry', 'Avocado']
+print(fruits)
+
+# Add
+fruits.append('Pineapple')
+print(fruits)
+
+# Pop
+fruits.pop()
+print(fruits)
+
+# Sort
+fruits.sort()
+print(fruits)
+
+# Insert
+fruits.insert(2, 'Coco')
+print(fruits)
+
