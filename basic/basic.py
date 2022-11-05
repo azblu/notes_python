1e59b70b462601fb5dd7aaf79c2b6a25eabc9425
azblu <98330304+azblu@users.noreply.github.com>
2022-11-04 22:57:22 -0500

    Update basic.py

--- basic/basic.py
+++ basic/basic.py
@@ -2,3 +2,29 @@ value = True
 print(value)
 print(not value)
 print(not(not value))
+
+
+
+
+#Indexing y slicing
+
+text = "Ella sabe Python"
+print(text[0])
+print(text[1])
+# print(text[999])
+size = len(text)
+print('size => ',size)
+print(text[size - 1])
+print(text[-1])
+
+## slicing
+
+print(text[0:5])
+print(text[10:16])
+print(text[:10])
+print(text[5:-1])
+print(text[5:])
+print(text[:])
+print(text[10:16:1])
+print(text[10:16:2])
+print(text[::2])
