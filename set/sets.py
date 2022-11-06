f1598039e043e7b3dbed9664cce13ea5fc8282b9
azblu <98330304+azblu@users.noreply.github.com>
2022-11-06 14:31:51 -0500

    Update sets.py

--- set/sets.py
+++ set/sets.py
@@ -4,12 +4,38 @@ sepB = {'+', '-', '/', '*'}
 print(sepA, sepB)
 # Union
 print(sepA | sepB)
+print(sepA.union(sepB))
+
+
 # Intersecction
 print(sepA & sepB)
+print(sepA.intersection(sepB))
+
 # Diference
 print(sepA - sepB)
-# the elements are both set
+print(sepA.difference(sepB))
+# symmetric difference
 print(sepA ^ sepB)
+print(sepA.symmetric_difference(sepB))
 
 res = {x for x in sepA if x not in sepB }
 print(res)
+
+# Operators
+# Add
+sepA.add('$')
+print(sepA)
+
+#update
+sepA.update({'8', '?'}) 
+print(sepA)
+
+#Remove
+sepA.remove('{') 
+print(sepA)
+sepA.discard('}')
+print(sepA)
+
+# Clear
+sepA.clear()
+print(sepA)
