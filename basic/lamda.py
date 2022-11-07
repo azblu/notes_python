a1eae34b84b9a376b22dc8b0b93a2ab9972316f6
azblu <98330304+azblu@users.noreply.github.com>
2022-11-07 15:21:03 -0500

    Create lamda.py

---
+++ basic/lamda.py
@@ -0,0 +1,14 @@
+fullname = lambda x, y : f'{x} {y}'
+print(fullname('Sam', 'Johnson'))
+
+
+names = ['Sam', 'Paula', 'Valentina', 'Justin']
+lastnames = ['Smith', 'Johnson', 'Williams', 'Brown']
+print(list(map(lambda x, y : f'{x} {y}', names, lastnames)))
+
+
+suma = lambda x, y: x+y
+resta = lambda x, y: x-y
+suma_resta = lambda x, y, z, p: suma(x, y) + resta(z, p)
+
+print(suma_resta(4,6,8,2))
