883e948b528d64f5605af8423499d3981d3be1d2
azblu <98330304+azblu@users.noreply.github.com>
2022-11-07 15:28:31 -0500

    Update lamda.py

--- basic/lamda.py
+++ basic/lamda.py
@@ -10,5 +10,8 @@ print(list(map(lambda x, y : f'{x} {y}', names, lastnames)))
 suma = lambda x, y: x+y
 resta = lambda x, y: x-y
 suma_resta = lambda x, y, z, p: suma(x, y) + resta(z, p)
-
 print(suma_resta(4,6,8,2))
+
+a = [1, 5, 8]
+b = [7, 9 , 2, 3]
+print(list(map(lambda x, y: x+y, a, b)))
