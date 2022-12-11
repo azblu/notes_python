077ddbc2dfc7ed32c5cf2badbed52d6bf83ea2ee
azblu <98330304+azblu@users.noreply.github.com>
2022-12-10 22:33:03 -0500

    Update yield.py

--- basic/yield.py
+++ basic/yield.py
@@ -23,3 +23,18 @@ def CB():
     print(num)
 
 CB()
+
+#Example 3
+def nextSquare():
+  i = 1
+  while True:
+    yield i * 1
+    i = i+1
+
+def CB():
+  for num in nextSquare():
+    if num > 100:
+      break
+    print(num)
+
+CB()
